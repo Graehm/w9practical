20b8f101220a732455db4a82dabfd6bb0afc12ac
Graehm <gpatrellfazio@gmail.com>
2023-05-31 17:34:03 -0500

    pt.2

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const app = express();
 
-app.get('/greeting/:name', function (req, res) {
-    res.send('<h1>hello stranger!</h1>')
-})
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Express</h1>');
+  });
 
 
 app.listen(3000, function () {
