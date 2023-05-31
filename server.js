0f1655a5aa68816c8d0d320ffb1c6502b714b344
Graehm <gpatrellfazio@gmail.com>
2023-05-31 18:15:46 -0500

    Greeting express application created.

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello Stranger')
+    res.send('<h1>Hello Stranger</h1>')
 })
-
+app.get('/greeting/:name', (req, res) => {
+    res.send(req.params); 
+    res.send(`Hello there, ${name}`);
+})
 app.listen(3000, () =>{
     console.log('Yes, I am listening on port 3000')
 })
